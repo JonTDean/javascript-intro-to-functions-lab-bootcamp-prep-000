370408aa63c1fd5a63b221e5f977fe24c5e8301a
Jonathan T Dean <jonathantomdean@gmail.com>
2020-01-11 03:07:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function shout(string){
-  
+  return console.log("This is not shouting!")
 }
 
 function whisper(string){
-  
+  return console.log("THIS IS NOT WHISPERING!")
 }
 
 function logShout(string){
-  
+  return console.log(
 }
 
 function logWhisper(string){
