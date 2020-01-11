7240758c3f04f36fd55d2dd431e6e15a55782f4b
Jonathan T Dean <jonathantomdean@gmail.com>
2020-01-11 03:15:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
  
- if(string == whisper) {
+ if(string == 'hello') {
    return "I can't hear you!"
    
- }else if(string == shout){
+ }else if(string == "HELLO"){
    return "YES INDEED!"
    
  }else if (string == "I love you, Grandma."){
